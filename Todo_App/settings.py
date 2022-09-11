fa12351483c7e561d63f9461fa3d28a92fac54d8
Speedoo1 <58296620+Speedoo1@users.noreply.github.com>
2022-09-11 20:31:11 +0100

    Update settings.py

--- Todo_App/settings.py
+++ Todo_App/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gkyrl6l+cg^224@y!gl=m9yz32l+@i!hgzid2^!=mevm!6#x33'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['todowebsite-production.up.railway.app/']
 
